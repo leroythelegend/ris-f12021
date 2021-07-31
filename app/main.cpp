8924817f7685a22e33bcb48afdd5096d44d13f0f
leroy <leroythelegend@gmail.com>
2021-07-31 19:45:03 +1000

    capture other packets than Event

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,12 @@
 #include "../inc/networkudp.h"
+#include "../inc/packetheader.h"
 
 #include <iostream>
 #include <fstream>
 #include <string>
 
 using namespace std;
+using namespace ris;
 
 int main(int argc, char const *argv[])
 {
@@ -16,9 +18,16 @@ int main(int argc, char const *argv[])
     // then I will subclass a NetworkFile from Network to test
     // decoders
 
-    // ofstream outfile("/tmp/f12021.out", ios::out|ios::binary);
-    // cout << "captured size " << bytes.size() << endl;
-    // outfile.write(reinterpret_cast<const char *>(bytes.data()), bytes.size());
-
+    // maybe able to get rid of the expicit pos?
+    // for now don't capture the event packet because
+    // we already have one but we will capture the other 
+    // parts of the event packet later.
+    Pos pos = 0;
+    if (PacketHeader(bytes, pos).value(PacketHeader::PACKETID).at(0).UInt != (unsigned int)PacketID::Event)
+    {
+        ofstream outfile("/tmp/f12021.out", ios::out|ios::binary);
+        cout << "captured size " << bytes.size() << endl;
+        outfile.write(reinterpret_cast<const char *>(bytes.data()), bytes.size());
+    }
     return 0;
 }
--- inc/definitions.h
+++ inc/definitions.h
@@ -7,4 +7,20 @@ namespace ris
 {
     using Bytes = std::vector<unsigned char>;
     using Pos = unsigned int;
+
+    enum class PacketID
+    {
+        Motion,
+        Session,
+        Lap_Data,
+        Event,
+        Participants,
+        Car_Setups,
+        Car_Telemetry,
+        Car_Status,
+        Final_Classification,
+        Lobby_Info,
+        Car_Damage,
+        Session_History
+    };
 } // namespace ris
